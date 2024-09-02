7d28c139c2a32e57bcdb8ca16910b4ecb947a888
RakaAF123 <110679603+RakaAdmiharfan@users.noreply.github.com>
2024-09-02 15:26:39 +0700

    restart

--- website_sdr/website_sdr/urls.py
+++ website_sdr/website_sdr/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for sdrweb project.
+URL configuration for website_sdr project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
